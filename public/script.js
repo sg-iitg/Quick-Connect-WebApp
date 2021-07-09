a3829186a12d4219f5d8d4990913283afa00f47f
Shreya Goel <shreya.goel@iitg.ac.in>
2021-07-09 22:28:25 +0530

    Added TURN server

--- public/script.js
+++ public/script.js
@@ -6,10 +6,15 @@ const videoGrid = document.getElementById('video-grid')
 let configuration = {
     'iceServers': [
         {
-        "urls": ["stun:stun.l.google.com:19302", 
-        "stun:stun1.l.google.com:19302", 
-        "stun:stun2.l.google.com:19302"]
-        }
+            "urls": ["stun:stun.l.google.com:19302", 
+            "stun:stun1.l.google.com:19302", 
+            "stun:stun2.l.google.com:19302"], 
+        },
+        {
+            'url': 'turn:numb.viagenie.ca',
+            'credential': 'muazkh',
+            'username': 'webrtc@live.com'
+        },
     ]
 }
 
