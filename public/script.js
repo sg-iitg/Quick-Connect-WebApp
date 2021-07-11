3779e677bba4e92ab691120e5eade30899198add
Shreya Goel <shreya.goel@iitg.ac.in>
2021-07-11 16:12:33 +0530

    Removed TURN server

--- public/script.js
+++ public/script.js
@@ -8,8 +8,8 @@ let configuration = {
         {
             "urls": ["stun:stun.l.google.com:19302", 
             "stun:stun1.l.google.com:19302", 
-            "stun:stun2.l.google.com:19302"], 
-        },
+            "stun:stun2.l.google.com:19302"]
+        }
     ]
 }
 
