e90beb1b01866201832b998f19bece2916c64acf
Shreya Goel <shreya.goel@iitg.ac.in>
2021-07-11 16:05:50 +0530

    Removed TURN server

--- public/script.js
+++ public/script.js
@@ -10,11 +10,6 @@ let configuration = {
             "stun:stun1.l.google.com:19302", 
             "stun:stun2.l.google.com:19302"], 
         },
-        {
-            url: 'turn:numb.viagenie.ca',
-            credential: 'muazkh',
-            username: 'webrtc@live.com'
-        },
     ]
 }
 
